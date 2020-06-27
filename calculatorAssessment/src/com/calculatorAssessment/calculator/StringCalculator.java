91f36bc028846d29fa7e77d9059ef9aa37ad738b
Aurgho Aich <Aurgho Aich@LAPTOP-SC5SFOP4>
2020-06-27 21:59:30 +0530

    StringCalculator should return the sum of two numbers solved.

--- calculatorAssessment/src/com/calculatorAssessment/calculator/StringCalculator.java
+++ calculatorAssessment/src/com/calculatorAssessment/calculator/StringCalculator.java
@@ -5,8 +5,17 @@ public class StringCalculator {
 	public static int add(String data) {
 		if (data.isEmpty())
 			return 0;
+		else if(data.contains(","))
+		{
+			String[] numbers = data.split(",");
+			return StringtoInt(numbers[0])+StringtoInt(numbers[1]);
+		}
 		else
-			return Integer.parseInt(data);
+			return StringtoInt(data);
+	}
+	
+	private static int StringtoInt(String num) {
+		return Integer.parseInt(num);
 	}
 
 }
