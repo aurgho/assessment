2eb944d28c95cc1de37b2fd4942a4e3a2f368669
Aurgho Aich <Aurgho Aich@LAPTOP-SC5SFOP4>
2020-06-27 21:34:34 +0530

    StringCalculator returning 0 for empty string

---
+++ calculatorAssessment/src/com/calculatorAssessment/calculator/StringCalculator.java
@@ -0,0 +1,9 @@
+package com.calculatorAssessment.calculator;
+
+public class StringCalculator {
+	
+	public static int add(String data) {
+		return -1;
+	}
+
+}
---
+++ calculatorAssessment/src/com/calculatorAssessment/calculatorTest/StringCalculatorTest.java
@@ -0,0 +1,16 @@
+package com.calculatorAssessment.calculatorTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.calculatorAssessment.calculator.StringCalculator;
+
+public class StringCalculatorTest {
+	
+	@Test
+	public void returnZeroForEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
+
+}
